c3f5956876440367420cbde64b6bb3344cfcf673
vangheem <vangheem@gmail.com>
2015-10-04 13:56:32 -0500

    add patch to generating scales

--- plone4/csrffixes/patches.py
+++ plone4/csrffixes/patches.py
@@ -1,3 +1,4 @@
+from zope.globalrequest import getRequest
 from plone.protect.interfaces import IDisableCSRFProtection
 from zope.interface import alsoProvides
 
@@ -5,3 +6,10 @@ from zope.interface import alsoProvides
 def disable_csrf(self, *args, **kwargs):
     alsoProvides(self.request, IDisableCSRFProtection)
     return self._old___call__(*args, **kwargs)
+
+
+def createScale(self, *args, **kwargs):
+    req = getRequest()
+    if req:
+        alsoProvides(req, IDisableCSRFProtection)
+    return self._old_createScale(*args, **kwargs)
